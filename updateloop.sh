6b4080f59a48498718f0fab37928b91266ca5ed6
myrlyn <myrlyn@localhost.localdomain>
2021-07-07 14:54:42 -0400

    minimize

--- updateloop.sh
+++ updateloop.sh
@@ -6,5 +6,5 @@ sleep 28800
 echo starting freshclam
 /usr/bin/freshclam
 echo reloading virus definitions
-/usr/bin/kill-s SIGUSR2 `cat /run/clamd.scan/clamd.pid`
+/usr/bin/kill -s SIGUSR2 `cat /run/clamd.scan/clamd.pid`
 done
