242ac1120b257ba61c0e43f93857fe9cb396ac90
myrlyn <myrlyn@users.noreply.github.com>
2021-07-07 14:28:58 -0400

    Update entrypoint.sh
    removed commented code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 /usr/bin/dnf -y upgrade clamd clamav-update
 /usr/bin/freshclam
-#/bin/bash /usr/bin/updateloop.sh   &    
-#/usr/sbin/clamd -F -c /etc/clamd/scan.conf
 /usr/bin/entrypoint.py
